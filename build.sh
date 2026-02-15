9207896769070ee39c8bbdfe5e5fb0fe650190d4
zn-har <zenharmuhammed00@gmail.com>
2026-02-15 16:17:36 +0530

    fix3 to v1

--- build.sh
+++ build.sh
@@ -4,6 +4,5 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py makemigrations game admin
 python manage.py migrate
 python manage.py seed_tasks
